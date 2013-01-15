f20bfc9b081be1ff35537975ba921375ab9cb664
night.kame <night.kame@gmail.com>
2013-01-15 02:47:39 +0100

    Va te coucher...

--- test/src/nk/questions/antlr/ArithmetiqueCalcTest.java
+++ test/src/nk/questions/antlr/ArithmetiqueCalcTest.java
@@ -17,7 +17,7 @@ public class ArithmetiqueCalcTest {
 	public void test() throws IOException, RecognitionException {
 		evalue("(1+2)*2", new BigDecimal(6.));
 		evalue("(1+4)/2", new BigDecimal(2.5));
-		evalue("((1,1+2)+3,14+4+(5+6+7)+(8+9+10)*4267387833344334647677634)/2*553344300034334349999000",
+		evalue("((1.1+2)+3.14+4+(5+6+7)+(8+9+10)*4267387833344334647677634)/2*553344300034334349999000",
 				new BigDecimal("31878018903828899277492024491376690701584023926880"));
 	}
 
